f2739adb7fbfbf4e1dc427bd420d58afe66ac1e4
Kevin Koech <kevinkoech@proton.me>
2023-07-03 23:25:59 +0300

    creates 5 zombie processes

---
+++ 0x05-processes_and_signals/102-zombie.c
@@ -0,0 +1,50 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+
+/**
+ * infinite_while - a while loop that
+ * runs infinitely and sleeps for 1
+ * second in each iteration
+ *
+ * Return: 0
+ *
+ */
+
+
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+
+	return (0);
+}
+
+/**
+ * main - creates 5 zombie processes
+ *
+ * Return: 0 if successful
+ *
+ */
+
+int main(void)
+{
+	int count = 0;
+	pid_t child;
+
+	for (count = 0; count < 6; count++)
+	{
+		child = fork();
+		if (child > 0)
+			printf("Zombie process created, PID: %d\n", chil);
+		else
+			exit(EXIT_FAILURE);
+	}
+
+	infinite_while();
+
+	return (0);
+}
+
