7433a919c1461879e6649c756aaad8e7cd117ec3
Kevin Koech <kevinkoech@proton.me>
2023-10-23 00:27:07 +0200

    modify to return employee name

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -2,7 +2,7 @@
 
 """
 This file contains a function that calls a fake
-api and displays dummy data.
+API and displays dummy data.
 """
 
 # Importing libraries
@@ -11,7 +11,28 @@ import json
 import sys
 
 
+def fetch_employee_name(employee_id):
+    # Define the API endpoint to fetch the employee name
+    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
+
+    # Send a GET request to the API to fetch the user information
+    response = requests.get(user_url)
+
+    if response.status_code != 200:
+        return None
+
+    user_data = response.json()
+    return user_data.get('name', f'Employee {employee_id}')
+
+
 def get_employee_todo_progress(employee_id):
+    # Get the employee name
+    employee_name = fetch_employee_name(employee_id)
+
+    if employee_name is None:
+        print(f"Error: Unable to fetch data for employee {employee_id}")
+        return
+
     # Define the API endpoint with the employee_id
     url = f'https://jsonplaceholder.typicode.com/users/{employee_id}/todos'
 
@@ -19,20 +40,17 @@ def get_employee_todo_progress(employee_id):
     response = requests.get(url)
 
     if response.status_code != 200:
-        print(f"Error: Unable to fetch data for employee {employee_id}")
+        print(f"Error: Unable to fetch TODO data for employee {employee_id}")
         return
 
     todos = response.json()
-
-    # Get the employee name
-    employee_name = todos[0]['userId']
 
     # Calculate the number of completed tasks and total tasks
     completed_tasks = [task for task in todos if task['completed']]
     completed = len(completed_tasks)
     total_tasks = len(todos)
 
-    print("Employee {} is done with tasks {}/{}: "
+    print("Employee {} is done with tasks {}/{}:"
           .format(employee_name, completed, total_tasks))
 
     # Print the titles of completed tasks
